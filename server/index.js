3addbf1dd0f784319973202f56c8d6e9f498a18c
Mahad Daood <mohummadmahad@gmail.com>
2020-04-27 15:09:18 +0500

    yo

--- server/index.js
+++ server/index.js
@@ -45,15 +45,15 @@ app.use('/api/product', require('./routes/product'));
 app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static('client/build'));
 
-  // index.html for all page routes
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-  });
-}
+// index.html for all page routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+});
+// }
 
 const port = process.env.PORT || 8080;
 
