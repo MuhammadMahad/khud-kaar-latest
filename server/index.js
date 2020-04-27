5e8b97928bfcc773c950f376fb821bd4ff4efd84
Mahad Daood <mohummadmahad@gmail.com>
2020-04-27 15:22:09 +0500

    yo

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.use('/uploads', express.static('uploads'));
 // Serve static assets if in production
 // if (process.env.NODE_ENV === 'production') {
 // Set static folder
-app.use(express.static('client/build'));
+app.use(express.static(path.resolve(__dirname, '../client', 'build')));
 
 // index.html for all page routes
 app.get('*', (req, res) => {
