5fc9c05eb8e59db88d7ea51802634144816b6021
Mahad Daood <mohummadmahad@gmail.com>
2020-05-07 08:30:10 +0500

    basic draw -> menu

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -42,7 +42,7 @@ function NavBar() {
           <Icon type='align-right' />
         </Button>
         <Drawer
-          title='Basic Drawer'
+          title='Menu'
           placement='right'
           className='menu_drawer'
           closable={false}
